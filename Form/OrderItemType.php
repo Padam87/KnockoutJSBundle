0e1dbb562780382a81b2c7c81b2b55cc6e1f6107
Padam87 <prager.adam87@gmail.com>
2012-11-15 02:28:28 +0100

    Update to Symfony2.1.x, fixes

---
+++ Form/OrderItemType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Padam87\KnockoutJSBundle\Form;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+class OrderItemType extends \Padam87\BaseBundle\Form\OrderItemType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+	
+	public function getDefaultOptions(array $options)
+	{
+		return array(
+            'data_class' => 'Padam87\BaseBundle\Entity\OrderItem'
+		);
+	}
+
+    public function getName()
+    {
+        return 'order';
+    }
+}
