24064f61bcecc174137d7e05e57de8b87c0f51a7
Adam Prager <prager.adam87@gmail.com>
2013-02-03 06:17:26 +0100

    works without generating the entire form

---
+++ Command/GenerateCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Padam87\KnockoutJSBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('knockout:generate')
+            ->setDescription('Generates a VM for the given form')
+            ->addArgument('form', InputArgument::REQUIRED, 'namespaced name of the form')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $kernel = $this->getApplication()->getKernel();
+        $container = $kernel->getContainer();
+        $formName = $input->getArgument('form');
+        $form = $container->get('form.factory')->create(new $formName());
+        $view = $form->createView();
+        
+        $twigKo = $container->get('twig.extension.knockout');
+        $twigKo->initRuntime($container->get('twig'));
+        
+        $vM = $twigKo->createViewModel($view->vars['knockout'], false);
+        $dirname = $kernel->getRootDir() . '/Resources/knockout';
+        $filename = $dirname . '/' . $view->vars['name'] . '.ko.js';
+        
+        if (!is_dir($dirname)) {
+            mkdir($dirname);
+        }
+        
+        file_put_contents($filename, $vM);
+    }
+}
---
+++ Twig/Extension/KnockoutExtension.php
@@ -0,0 +1,50 @@
+<?php
+namespace Padam87\KnockoutJSBundle\Twig\Extension;
+
+use Symfony\Bridge\Twig\TokenParser\FormThemeTokenParser;
+use Symfony\Bridge\Twig\Form\TwigRendererInterface;
+use Symfony\Component\Form\Extension\Core\View\ChoiceView;
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * @DI\Service("twig.extension.knockout")
+ * @DI\Tag("twig.extension")
+ */
+class KnockoutExtension extends \Twig_Extension
+{
+    public $environment;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function initRuntime(\Twig_Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            'knockout'   => new \Twig_Function_Method($this, 'createViewModel'),
+        );
+    }
+    
+    public function createViewModel($params, $script = true)
+    {
+        $knockout = $this->environment->render("Padam87KnockoutJSBundle::knockout.js.twig", $params);
+        
+        if ($script === true) {
+            return '<script>' . $knockout . '</script>';
+        }
+        
+        return $knockout;
+    }
+
+    public function getName()
+    {
+        return 'knockout';
+    }
+}
